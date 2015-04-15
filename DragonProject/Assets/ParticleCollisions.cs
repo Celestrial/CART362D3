bd5b995cd0be93e21c5629fc7fceef7d2ec17b38
celestrial <sean.celestrial@gmail.com>
2015-04-15 00:36:31 -0400

    no collision, and camera does not circle. Maybe add a second camera tomorow for a front view

---
+++ DragonProject/Assets/ParticleCollisions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleCollisions : MonoBehaviour {
+	public GameObject fire;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnParticleCollision(GameObject other)
+	{
+		Instantiate(fire);
+	}
+}
--- DragonProject/Assets/Scripts/FireBreathScript.cs
+++ DragonProject/Assets/Scripts/FireBreathScript.cs
@@ -5,6 +5,7 @@ public class FireBreathScript : MonoBehaviour {
 	Animation actions;
 	public GameObject fire;
 	public GameObject gums;
+	float fireControl = 0;
 	GameObject fireBreath = null;
 	// Use this for initialization
 	void Start () {
@@ -13,10 +14,11 @@ public class FireBreathScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButton("Fire1"))
+		if(Input.GetButton("Fire1") && fireControl > .5)
 		{
 			fireBreath = (GameObject)Instantiate(fire, gums.transform.position, transform.rotation);
 			actions.Play("FireGlide");
+			fireControl = 0;
 		}
 		if(!actions.isPlaying)
 		{
@@ -26,6 +28,6 @@ public class FireBreathScript : MonoBehaviour {
 		if(fireBreath != null)
 			fireBreath.transform.position = GameObject.Find("FireOut").transform.position;
 
-
+		fireControl+= Time.deltaTime;
 	}
 }
